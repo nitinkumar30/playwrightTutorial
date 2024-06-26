061eecfb1a107948a7eafb35a52881aafeb8a5cf
Nitin Kumar <nitinkumarpythonic@gmail.com>
2024-06-26 14:39:53 +0530

    Login successful with some exceptions and validations.

--- demo_1.py
+++ demo_1.py
@@ -3,8 +3,8 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+    browser = playwright.chromium.launch(headless=False, slow_mo=500, args=["--start-maximized"])
+    context = browser.new_context(no_viewport=True)
     page = context.new_page()
     page.wait_for_load_state('networkidle')
     page.goto("https://practicetestautomation.com/practice-test-login/")
@@ -13,8 +13,19 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Username").fill("student")
     page.get_by_label("Username").press("Tab")
     page.get_by_label("Password").fill("Password123")
-    page.get_by_role("button", name="Submit").click()
+    try:
+        page.get_by_role("button", name="Submit").click(timeout=3000)
+        print("User logged in successfully!")
+    except playwright._impl._errors.TimeoutError:
+        print("Timeout occurred while clicking the submit button.")
 
+    # page.pause()
+
+    try:
+        expect(page.get_by_role("link", name="Log out")).to_be_visible()
+        print("Logout button is visible!")
+    except playwright._impl._errors.TimeoutError:
+        print("Logout button is not visible within the timeout.")
     # ---------------------
     context.close()
     browser.close()
